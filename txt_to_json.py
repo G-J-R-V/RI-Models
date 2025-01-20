390a9b6bde3057f90923f86f58accdd53754bf90
João Vitor <joaovnevescosta@hotmail.com>
2025-01-20 13:06:20 -0300

    lista com stopwords stemming

--- txt_to_json.py
+++ txt_to_json.py
@@ -1,10 +1,15 @@
 import json
+import nltk
+from nltk.stem import SnowballStemmer
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
 
 def get_json():
     with open(r"res/loja_de_roupas_ultimos_itens.txt",'r',encoding='utf-8') as arquivo:
         linhas = arquivo.readlines()
 
-    dicionario = {}
+    lista = []
     d = {
         'Item':'',
         'Nome':'',
@@ -13,7 +18,6 @@ def get_json():
     }
     ctrl = 0
     for l in linhas:
-        print(l)
         if ctrl==0:
             d['Item'] = l.replace('Item ','').replace('\n','')
         elif ctrl==1:
@@ -25,13 +29,30 @@ def get_json():
 
         ctrl+=1
         if ctrl>=4:
-            dicionario["Doc"+d['Item']] = d.copy()
+            lista.append(d.copy())
             ctrl=0
     
     with open(r'res/dados_loja.json','w',encoding='utf-8') as arquivo_json:
-        json.dump(dicionario,arquivo_json,indent=4,ensure_ascii=False)
+        json.dump(lista,arquivo_json,indent=4,ensure_ascii=False)
 
+def tratar_desc():
+    stemmer=SnowballStemmer("portuguese")
+    sw= set(stopwords.words('portuguese'))
+    with open(r"res/dados_loja.json","r",encoding='utf-8') as arquivo:
+        dados=json.load(arquivo)
+
+    for i in dados:
+        #i['Descricao']=dados[i]['Descricao'].lower().replace('.','').replace(',','')
+        l = []
+        lista = i['Descricao'].lower().replace('.','').replace(',','').split()
+        lista=[w for w in lista if not w in sw]
+        for p in lista:
+            l.append(stemmer.stem(p))
+        i['Descricao']=l
     
+    with open(r'res/dados_loja.json','w',encoding='utf-8') as arquivo_json:
+        json.dump(dados,arquivo_json,indent=4,ensure_ascii=False)
 
 
 get_json()
+tratar_desc()
