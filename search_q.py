88ac1734e6ed80ccf63d81c9981a1ba1ae3b8bff
GNS03 <gabrielnogueirasouza@gmail.com>
2025-02-08 14:40:39 -0300

    Update search_q.py
    New Global Stemmer and Global test_data_dict, clean_query, search_query

--- search_q.py
+++ search_q.py
@@ -1,14 +1,59 @@
 import json
+import string
 
-import Models.vetorial
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+
 from Models.vetorial import modelo_vetorial
 
-def search_query(query:str = ''):
-    with open("res/dados_loja.json", "r") as file:
-        test_data = json.load(file)
+### Global Stemmer
+nltk.download("stopwords")
 
-    return modelo_vetorial(test_data, query)
+stemmer = SnowballStemmer("portuguese")
+sw = set(stopwords.words("portuguese"))
+###
+
+### Global test_data
+with open("./res/dados_loja.json", "r", encoding="utf-8") as file:
+    test_data = json.load(file)
+
+test_data_dict = {}
+
+for i, value in enumerate(test_data):
+    value["R"] = False
+    test_data_dict[f"Doc{i}"] = value
+###
+
+
+def clean_query(query: str = "") -> list[str]:
+    global stemmer
+    global sw
+
+    query = [
+        stemmer.stem(w)
+        for w in query.translate(str.maketrans("", "", string.punctuation)).split(" ")
+        if w not in sw
+    ]
+
+    print("Clean query: " + f"{query}" + "\n")
+
+    return query
+
+
+def search_query(query: str = ""):
+
+    global test_data_dict
+
+    cleaned_query = clean_query(query)
+
+    resultado_vetorial = modelo_vetorial(test_data_dict, cleaned_query)
+
+    return resultado_vetorial  ##
 
 
 if __name__ == "__main__":
-    search_query("Camisa vermelha")
+
+    resultado = search_query("Blusa Azul Escuro")
+
+    print(f"\nResultado: {resultado}")
